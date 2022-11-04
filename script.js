cb4920c3465173fa085951094746479929f9951b
LauriJokinen <91256944+LauriJokinen@users.noreply.github.com>
2022-11-04 21:50:10 +0200

    paljon kaikkea

--- script.js
+++ script.js
@@ -1,26 +1,48 @@
+window.onload = (event) => {
 
+for ( var i = 0, len = localStorage.length; i < len; ++i ) {
+
+  const varasto = document.createElement("li")
+  varasto.textContent =  localStorage.key(i);
+  document.querySelector("#ostokset").appendChild(varasto);
+
+}
+}
+
+for ( var i = 0, len = localStorage.length; i < len; ++i ) {
+  console.log( localStorage.key(i ) );
+}
 
 
 document.querySelector("#kirjoita").addEventListener("keydown",(event)=> {
-
-
-
    if (event.key == "Enter" ){
       addItem();
+      }
+    });
 
-  }
+  //lisää listalle artikkelin
 
+document.querySelector("#lisää").addEventListener("click", (event) => {
+    addItem();
+})
+
+ //poistaa kaikki artikkelit listalta
+
+document.querySelector("#poista").addEventListener("click", (event) => {
+  document.querySelector("#ostokset").textContent ="";
+  localStorage.clear();
 });
-
 
   //addItem funktio lisää tekstin sivulle, tyhjentää tekstikentän ja mahdollistaa
   //ostosten yliviivauksen tai sen poiston
 
 addItem = () => {
 
-  const item = document.createElement("h2")
+  const item = document.createElement("li")
   item.textContent = document.querySelector("#kirjoita").value;
 
+
+  //Klikkaamalla värinvaihto ja yliviivaus, toisella klikkauksella palaa takaisin alkupetäiseen.
 
   item.addEventListener("click", () => {
     if(item.style.textDecoration != "line-through" && item.style.color !="grey"){
@@ -33,8 +55,16 @@ addItem = () => {
     }
   })
 
+  //Ostosten tulostus, liian vähistä merkeistä hälyttää
+
   if(item.textContent.length >= 3) {
   document.querySelector("#ostokset").appendChild(item);
+
+
+    localStorage.setItem(item.textContent, "ostos");
+
+
+
   document.querySelector("#kirjoita").value = null;
 }
     else if (item.textContent.length < 3) {
